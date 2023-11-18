980e52fed60a0bc80f29dba2a54915c40a9eb102
Bobby <williamcheungyam123@gmail.com>
2023-11-18 17:28:01 +0800

    updata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -119,16 +119,3 @@
   </button>
   </div>
 </section>
-
-<svg
-data-te-animation-init
-data-te-animation-reset="true"
-data-te-animation="[slide-right_1s_ease-in-out]"
-xmlns="http://www.w3.org/2000/svg"
-viewBox="0 0 24 24"
-fill="currentColor"
-class="h-11 w-11">
-<path
-  d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
-</svg>
-@endsection
